c975f826451ebdd1bcc3d7aa9f92861cc3a4eb8c
laura <lauramedinanazareth@gmail.com>
2022-03-01 11:03:55 -0500

    talle1.js

---
+++ talle1.js
@@ -0,0 +1,23 @@
+//Archivo de práctica y pruebas de JavaScript
+/* Taller 1
+Fecha 21/02/2022
+Realizado por: Laura Medina
+*/ 
+var nombre;  
+console.log (nombre); //string
+nombre="laura";
+var grado;
+console.log (grado); //int
+var edad;
+console.log (edad); //int 
+var grado = "10b";
+console.log (grado);
+nombre= "Laura";console.log(nombre+" "+grado);
+var edad= 14;
+var edad = edad;
+var grado= prompt("ingrese el grado");
+console.log ("el grado es:" + grado);
+//imprimir por consola 
+//concatenado
+console.log ( "El estudiante " + nombre + " del grado " + gradoestudiante + " tiene " + edad + " años");
+
